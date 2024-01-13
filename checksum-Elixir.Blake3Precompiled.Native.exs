384a69cbe72602f0e6ad69110c61493f72a1fe41
madclaws <pavanan.anandu63@gmail.com>
2024-01-13 17:05:03 +0530

    feat: adding the checksum

---
+++ checksum-Elixir.Blake3Precompiled.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "blake3_precompiled-v0.1.6-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:c57f3eb49e5916c597826a3240ca016d03c9abd17bf7451836c2c398ee10e03d",
+  "blake3_precompiled-v0.1.6-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:e8a0b58d54c446dcae2e09aa5789d5812624deded4b3c9554bba104df0ae4aee",
+  "libblake3_precompiled-v0.1.6-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:0193443ae564ccf93913c443e1b05fec397885a8cd0e5ec1e42fb0922403aa4c",
+  "libblake3_precompiled-v0.1.6-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7a1270ebac72673fdb1a6a17e2b7a620c94e4e563025f63a7c8e4c4459126c77",
+  "libblake3_precompiled-v0.1.6-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:716fd04bd5c88a8ff2c4f34c04f5d5d59021febb525d9719f64f850286f43fd3",
+  "libblake3_precompiled-v0.1.6-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:bcb29e0ea289d772091ba17d4a44f1840a2b100ff4c5a016407fe6ba366da99b",
+  "libblake3_precompiled-v0.1.6-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:6274351563a1136de4616f2ab372999bd3177079fbfb914356278ab725f9251c",
+  "libblake3_precompiled-v0.1.6-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:40474765a5dc2f4ffe37b062d196f3e23799d5f5e3e98c4bddd06b80c0176cdc",
+  "libblake3_precompiled-v0.1.6-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b5039e5b26aa407b1066fac263938db5aecc7ebd11c9552eb1c3f0036b971205",
+  "libblake3_precompiled-v0.1.6-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:80e310c88a720e204e91056bae692e6e9e1cf6331d16aa9ddf18a8a826d67e34",
+}
