e1415c9fddaf086f5f2e6471a83169583accbba3
madclaws <pavanan.anandu63@gmail.com>
2024-02-01 08:27:06 +0530

    fix: added force_build nif

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,6 @@ config :blake3_precompiled, Blake3Precompiled.Native,
   mode: :release,
   features: Blake3Precompiled.Project.config_features()
 
+config :rustler_precompiled, :force_build, blake3_precompiled: true
+
 import_config("#{Mix.env()}.exs")
